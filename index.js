878886872a745cc84294f4f07c14b29290bbcd46
Michael Steiner <michaelvsteiner@gmail.com>
2021-09-30 13:56:00 -0600

    adds p tag

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ server.use(cors());
 server.use(express.json());
 
 server.get("/", (req, res) => {
-  res.send("<h1>Home Page</h1>");
+  res.send("<h1>Home Page</h1><p>/users should work too</p>");
 });
 
 server.get("/api/users", (req, res) => {
